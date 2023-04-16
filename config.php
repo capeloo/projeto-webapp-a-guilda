298b551ca47c60c57a5f306d180910c2f5afbb3f
capeloo <caiocapelo@alu.ufc.br>
2023-04-16 10:40:20 -0300

    config.php

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php 
+    /* Credenciais do banco de dados. Supondo que você esteja 
+    executando o MySQL servidor com configuração padrão 
+    (usuário 'root' sem senha) */
+    define('DB_SERVER', 'localhost');
+    define('DB_USERNAME', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME','meu_banco');
+
+    /* Tentativa de conexão com o banco de dados MySQL */
+    try {
+        
+    }
+?>
