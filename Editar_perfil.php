eee2b35fb9d23590f8a6b428ca71e794fed7ee46
capeloo <caiocapelo@alu.ufc.br>
2023-05-16 12:34:42 -0300

    up

--- Editar_perfil.php
+++ Editar_perfil.php
@@ -1,68 +1,38 @@
-<?php
-
-//Inicializa variáveis vazias
-$apelido_erro = $senha_erro = $login_erro = "";
-$apelido = $senha = $id = "";
-
-require_once "config.php";
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Obter os dados enviados pelo formulário
-    $apelido = $_POST['apelido'];
-    $senha = $_POST['senha'];
-    $id = $_POST['id'];
-    
-    // Conexão com o banco de dados
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "taverna";
-
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Falha na conexão: " . $conn->connect_error);
-    }
-
-    // Atualizar os dados na tabela 'usuario' ********
-    $sql = "UPDATE usuario SET apelido='$apelido' and senha='$senha' WHERE id='$id'";
-    if ($conn->query($sql) === TRUE) {
-        echo "<script>alert('Edição concluída com sucesso');</script>";
-        header("Location: Usuario_dashboard.php"); // Redirecionar para a página de dashboard após a edição
-        exit;
-    } else {
-        echo "Erro ao editar usuário: " . $conn->error;
-    }
-
-    $conn->close();
-
-}
-
-?>
-
-
-
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST">
-
-	<input type="hidden" name="acao" value="edita">
-	<div class="mb-3">
-		<label>Apelido</label>
-		<input type="text" name="apelido" value="<?php echo $apelido; ?>"  class="form-control" required>		
-	</div>
-	<div class="mb-3">
-		<label>Senha</label>
-		<input type="password" name="senha" class="form-control" value="<?php echo $senha; ?>" required>
-	</div>
-    <div class="mb-3">
-		<label>Confirmar senha</label>
-		<input type="password" name="confirmar_senha" class="form-control" value="<?php echo $senha; ?>" required>
-	</div>
-
-	<div class="mb-3">
-		<button type="submit" class="btn btn-primary">Enviar</button>
-	</div>
-
-</form>
-
-
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar perfil</title>
+</head>
+<body>
+    <h1>Editar perfil</h1>
+    <form action="" method="post">
+        <div>
+            foto    
+        </div>
+        <div>
+            nome
+        </div>
+        <div>
+            bio
+        </div>
+        <div>
+            email
+        </div>
+        <div>
+            celular
+        </div>
+        <div>
+            discord
+        </div>
+        <div>
+            matricula
+        </div>
+        <div>
+        <button class="btn btn-success" type="submit">Salvar</button>
+        </div>
+    </form>
+</body>
+</html>
