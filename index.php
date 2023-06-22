12cb6d3d9c974a53da77af3856ba80dcbb5d51a4
Caio Henrique Capêlo <108996602+capeloo@users.noreply.github.com>
2023-06-22 12:16:57 -0300

    up

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
     <!-- Chamando as folhas de estilo do Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/custom.css">
+    <link rel="stylesheet" href="css/media-queries.css">
 </head>
 <body>
   <!-- Barra de navegação -->
@@ -21,9 +22,11 @@
   </nav> 
 
 <!-- Conteúdo da página -->
-    <div class="row w-100" style="height: 35em;">
-      <div class="col" id="col1">
-        <h1 id="titulo">O seu hub de RPG, seguro e inclusivo.</h1>
+<main>
+  <section style="height: 70vh;">
+    <div class="container-fluid" id="canva" style="height: 70vh;">
+      <div  id="col1">
+        <h1 id="titulo">O seu hub de RPG seguro e inclusivo.</h1>
         <p id="texto">
           Crie ou entre em mesas, participe de conteúdos coletivos, encontre jogadores e personalize seu perfil em uma plataforma direto do seu navegador.
         </p>
@@ -33,8 +36,17 @@
           </div>
         </a>
       </div>
-      <div class="col" id="col2" style="background: url(assets/images/bussola.jpg); background-size: cover;"></div>
+      <div id="col2" style="background: url(assets/images/bussola.jpg); background-size: cover;"></div>
     </div>
+  </section>
+</main>
+
+<footer class="footer p-3">
+    <div class="container-fluid" id="foot">
+        <p id="p1">&copy; A Guilda. Siga em frente!</p>
+        <p class="text-end" id="p2">Siga-nos: <a href="https://www.instagram.com/aguilda_smd/" target="_blank"><img src="assets/images/Ellipse.svg" style="width: 2em; height: 1.5em;"></a></p>
+    </div>
+  </footer>
     <!-- Chamando os scripts do Bootstrap -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
   </body>
