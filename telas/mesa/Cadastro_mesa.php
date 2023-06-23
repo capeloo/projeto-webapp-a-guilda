71c41a76de1b31544dc2d5156dc0e3533c514aa2
capeloo <caiocapelo@alu.ufc.br>
2023-06-23 15:18:59 -0300

    Revert "Correção condicional nome da campanha"
    This reverts commit cf32663962607a26254da0a51e4b9aa918d06eb2.

--- telas/mesa/Cadastro_mesa.php
+++ telas/mesa/Cadastro_mesa.php
@@ -78,7 +78,7 @@
         if (empty(trim($_POST["nome_campanha"]))) {
             $nome_campanha_erro = "Por favor, dê um nome para a sua campanha.";
         } else {
-            $nome_campanha = trim($_POST["nome_campanha"]);
+            $nome_campanha = $trim($_POST["nome_campanha"]);
         }
 
         $sistema = trim($_POST["sistema"]);
